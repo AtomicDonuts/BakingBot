033a9bdb360cbedfb3c17883e51cc73aaf89308a
AtomicDonuts <39431742+AtomicDonuts@users.noreply.github.com>
2020-02-03 22:39:34 +0100

    Update BakingBot.js

--- BakingBot.js
+++ BakingBot.js
@@ -1,6 +1,14 @@
+// BakingBot 
+// bakingbot is a simple bot that will get "Speed Baking III" for you
+// since SB3 Achievements is basically based on luck and there isn't any real strategy ,it should be perfectly legit 
+
+
 var BakingBot
 if(!BakingBot) BakingBot = {};
 BakingBot.version = "0.001";
 BakingBot.gameVersion = "2.022";
 BakingBot.bakeryName = "BakingBot is helping ";
 BakingBot.delay = 0;
+
+var bakeryName = Game.bakeryName;
+Game.bakeryName = BakingBot.robotName+bakeryName; 
