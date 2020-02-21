b0904986100add96c253f390cd27ac4cb42f0a69
AtomicDonuts <39431742+AtomicDonuts@users.noreply.github.com>
2020-02-21 12:20:20 +0100

    Add files via upload

--- BakingBot.js
+++ BakingBot.js
@@ -1,17 +1,17 @@
-// BakingBot 
+// BakingBot
 // bakingbot is a simple bot that will get "Speed Baking III" for you
-// since SB3 Achievements is basically based on luck and there isn't any real strategy ,it should be perfectly legit 
-
+// since SB3 Achievements is basically based on luck and there isn't any real strategy ,it should be perfectly legit
 
 var BakingBot
 if(!BakingBot) BakingBot = {};
-BakingBot.version = "0.100";
+BakingBot.version = "0.300";
 BakingBot.gameVersion = "2.022";
 BakingBot.robotName = "BakingBot is helping ";
 BakingBot.WaitingTime = 0;
-BakingBot.AscendTimeWait = 15*60*1000 + 4000;
-BakingBot.InitialBakeryName = ""
-
+BakingBot.AscendTimeWait = 15*60*1000 + 10000;
+BakingBot.InitialBakeryName = "";
+BakingBot.LastGoldenShimmer = [];
+BakingBot.AttemptsNumber = 1;
 
 BakingBot.run = function(){
 	if (Game.Achievements["Speed baking III"].won) BakingBot.stopBot();
@@ -23,12 +23,12 @@ BakingBot.run = function(){
 	}
 	BakingBot.AutoClickBigCookie();
 	BakingBot.AutoClickGoldenCookie();
-	
+
 	BakingBot.ClickFrenzyShopping();
 	BakingBot.FrenzyShopping();
 	BakingBot.ObjectBuy();
 	BakingBot.UpgradeBuy();
-	
+
 }
 
 BakingBot.restart = function(){
@@ -37,14 +37,17 @@ BakingBot.restart = function(){
 	if(Game.prestige == 0 && !Game.OnAscend)
 		prestigeup = confirm("Press OK to Ascend\nYou will LOSE all the cookie in your bakery");
 	if(prestigeup && !Game.Achievements["Speed baking III"].won && !Game.OnAscend){
+		BakingBot.SavingList("failed");
+		BakingBot.AttemptsNumber++;
+		BakingBot.LastGoldenShimmer = [];
 		Game.Ascend(true);
 		BakingBot.SetWaitingTime(4);
 		return;
 	}
 	if(prestigeup && !Game.Achievements["Speed baking III"].won && Game.OnAscend){
 		Game.PickAscensionMode();
-		Game.nextAscensionMode = 1; 
-		Game.ConfirmPrompt(); 
+		Game.nextAscensionMode = 1;
+		Game.ConfirmPrompt();
 		Game.Reincarnate(true);
 	}
 }
@@ -75,6 +78,7 @@ BakingBot.AutoClickGoldenCookie = function(){
 	var max = Game.shimmers.length
 	for(var i = 0;i<max;i++){
 		Game.shimmers[0].pop();
+		BakingBot.UpdatingList();
 	}
 }
 
@@ -89,14 +93,14 @@ BakingBot.ObjectBuy = function(){
 
 BakingBot.UpgradeBuy = function(){
 	if(!Game.UpgradesById[75].bought	&& Game.UpgradesById[75].canBuy()	&& Game.UpgradesById[75].unlocked)	Game.UpgradesById[75].buy();
-	
+
 	if(!Game.UpgradesById[0].bought	&& Game.UpgradesById[0].canBuy()	&& Game.UpgradesById[0].unlocked)	Game.UpgradesById[0].buy();
 	if(!Game.UpgradesById[1].bought	&& Game.UpgradesById[1].canBuy()	&& Game.UpgradesById[1].unlocked)	Game.UpgradesById[1].buy();
 	if(!Game.UpgradesById[2].bought	&& Game.UpgradesById[2].canBuy()	&& Game.UpgradesById[2].unlocked)	Game.UpgradesById[2].buy();
-	
+
 	if(!Game.UpgradesById[7].bought	&& Game.UpgradesById[7].canBuy() && Game.UpgradesById[1].bought	&& Game.UpgradesById[7].unlocked)	Game.UpgradesById[7].buy();
 	if(!Game.UpgradesById[8].bought	&& Game.UpgradesById[8].canBuy() && Game.UpgradesById[2].bought	&& Game.UpgradesById[8].unlocked)	Game.UpgradesById[8].buy();
-  
+
 	if(!Game.UpgradesById[10].bought	&& Game.ObjectsById[2].amount >= 5 && Game.UpgradesById[10].canBuy()	&& Game.UpgradesById[10].unlocked) Game.UpgradesById[10].buy();
 }
 
@@ -125,7 +129,35 @@ BakingBot.ClickFrenzyShopping = function(){
 		if(cursors.amount < 30 && BakingBot.CanIBuyBTen(0)) cursors.buy(10);
 	}
 }
+//Progress handling
+BakingBot.UpdatingList = function() {
+	BakingBot.LastGoldenShimmer.push(Game.shimmerTypes.golden.last)
+}
 
+BakingBot.ListWOStorm = function(){
+	array = BakingBot.LastGoldenShimmer;
+	var list = []
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] == "multiply cookies" ) list.push("Lucky")
+		if (array[i] != "cookie storm drop") list.push(array[i])
+	}
+	BakingBot.LastGoldenShimmer = list
+}
+
+BakingBot.SavingList = function(WasThatASuccess) {
+	BakingBot.ListWOStorm();
+	var str = "BakingBot LogList\n"
+	str += "Attempt n." + BakingBot.AttemptsNumber + " " + WasThatASuccess + "\n"
+	str += "Attempt started: " + BakingBot.TimeConverter(Game.startDate)  + "\n"
+	str += "Cookie Baked: " + Beautify(Game.cookiesEarned) + "\n"
+	str += "Golden Cookies Clicked: "+ Game.goldenClicksLocal +"\n"
+	str += "Golden: "
+	for (var i = 0; i < BakingBot.LastGoldenShimmer.length; i++) {
+		str += BakingBot.LastGoldenShimmer[i]	+ ", "
+	}
+	var blob=new Blob([str],{type:'text/plain;charset=utf-8'});
+	saveAs(blob,'BakingBotLog_n'+ BakingBot.AttemptsNumber+'.txt');
+}
 
 //{Menu
 if(!BakingBot.Backup) BakingBot.Backup = {};
@@ -136,22 +168,22 @@ BakingBot.ConfigData = {};
 
 BakingBot.Disp = {};
 
-BakingBot.ConfigPrefix = 'BakingBotConfig';	
-	
+BakingBot.ConfigPrefix = 'BakingBotConfig';
+
 BakingBot.SaveConfig = function(config) {
   try {
     window.localStorage.setItem(BakingBot.ConfigPrefix, JSON.stringify(config));
   } catch (e) {}
-}	
-	
+}
+
 BakingBot.LoadConfig = function() {
   try {
     if (window.localStorage.getItem(BakingBot.ConfigPrefix) != null) {
 	  BakingBot.Config = JSON.parse(window.localStorage.getItem(BakingBot.ConfigPrefix));
-		
+
      // Check values
 	  var mod = false;
-		
+
 	  for (var i in BakingBot.ConfigDefault) {
 			if (typeof BakingBot.Config[i]==='undefined' || BakingBot.Config[i]<0 || BakingBot.Config[i]>=BakingBot.ConfigData[i].label.length) {
 				mod = true;
@@ -159,28 +191,28 @@ BakingBot.LoadConfig = function() {
 				BakingBot.PreConfig[i] = BakingBot.ConfigDefault[i];
 			}
 	  }
-		
-	  if (mod) 
+
+	  if (mod)
 			BakingBot.SaveConfig(BakingBot.Config);
-    } 
-		
+    }
+
 		else { // Default values
 	  BakingBot.RestoreDefault();
     }
   } catch (e) {}
-}	
-	
+}
+
 BakingBot.RestoreDefault = function() {
   BakingBot.Config = {};
 	BakingBot.PreConfig = {};
   BakingBot.SaveConfig(BakingBot.ConfigDefault);
   BakingBot.LoadConfig();
   Game.UpdateMenu();
-}	
+}
 
 BakingBot.ToggleConfig = function(config) {
   BakingBot.ToggleConfigUp(config);
-  l(BakingBot.ConfigPrefix + config).className = 
+  l(BakingBot.ConfigPrefix + config).className =
     BakingBot.Config[config]?'option':'option off';
 }
 
@@ -193,19 +225,18 @@ BakingBot.ToggleConfigUp = function(config) {
   BakingBot.SaveConfig(BakingBot.Config);
 }
 
-BakingBot.ConfigData.ClickSpeed = 
+BakingBot.ConfigData.ClickSpeed =
   {label: ['OFF','3','6','9','12','15','18'], desc: 'How many click in 1 sec'};
-	
-	
-BakingBot.ConfigDefault = {ClickSpeed: 2,};	
-BakingBot.LoadConfig();	
-	
+
+BakingBot.ConfigDefault = {ClickSpeed: 2,};
+BakingBot.LoadConfig();
+
 BakingBot.Disp.GetConfigDisplay = function(config) {
   return BakingBot.ConfigData[config].label[BakingBot.Config[config]];
-}	
-	
+}
+
 BakingBot.Disp.AddMenuPref = function() {
-	
+
   var header = function(text) {
 		var div = document.createElement('div');
 		div.className = 'listing';
@@ -216,27 +247,27 @@ BakingBot.Disp.AddMenuPref = function() {
 		div.textContent = text;
 		return div;
   }
-	
+
   var frag = document.createDocumentFragment();
-	
+
   var div = document.createElement('div');
-	
+
   div.className = 'title ' + BakingBot.Disp.colorTextPre + BakingBot.Disp.colorBlue;
-	
+
   div.textContent = 'BakingBot Options';
-	
+
   frag.appendChild(div);
-	
+
   var listing = function(config,clickFunc) {
 		var div = document.createElement('div');
 		div.className = 'listing';
 		var a = document.createElement('a');
 		a.className = 'option';
-		if (BakingBot.Config[config] == 0) 
+		if (BakingBot.Config[config] == 0)
 			a.className = 'option off';
 		a.id = BakingBot.ConfigPrefix + config;
-		a.onclick = function() { 
-			BakingBot.ToggleConfig(config); 
+		a.onclick = function() {
+			BakingBot.ToggleConfig(config);
 		};
 		if (clickFunc) a.onclick = clickFunc;
 			a.textContent = BakingBot.Disp.GetConfigDisplay(config);
@@ -246,39 +277,44 @@ BakingBot.Disp.AddMenuPref = function() {
 		div.appendChild(label);
 		return div;
   }
-	
+
   frag.appendChild(listing('ClickSpeed',null));
   l('menu').childNodes[2].insertBefore(frag, l('menu').childNodes[2].childNodes[l('menu').childNodes[2].childNodes.length - 1]);
 
 }
-	
+
 if (!BakingBot.Backup.UpdateMenu) BakingBot.Backup.UpdateMenu = Game.UpdateMenu;
 
 Game.UpdateMenu = function() {
   BakingBot.Backup.UpdateMenu();
-  if (Game.onMenu == 'prefs') 
+  if (Game.onMenu == 'prefs')
 		BakingBot.Disp.AddMenuPref();
-}	
-//}	
+}
+//}
+
+//{ BakingSesame
+BakingBot.BakingSesame = function(){
+	//WIP
+	}
+//}
 
 //Utilities
-
-BakingBot.notify = function(BakingText) { 
-  console.log("BakingBot: "+BakingText); 
-  Game.Notify("BakingBot News",BakingText,[14,5],100); 
+BakingBot.notify = function(BakingText) {
+  console.log("BakingBot: "+BakingText);
+  Game.Notify("BakingBot News",BakingText,[14,5],100);
 }
 
 BakingBot.RenameBakery = function(){
-	if (Game.bakeryName.slice(0,BakingBot.robotName.length)!=BakingBot.robotName) { 
+	if (Game.bakeryName.slice(0,BakingBot.robotName.length)!=BakingBot.robotName) {
 		BakingBot.InitialBakeryName = Game.bakeryName;
 		var bakeryName = Game.bakeryName;
-		Game.bakeryName = BakingBot.robotName+bakeryName; 
-		Game.bakeryNamePrompt(); 
+		Game.bakeryName = BakingBot.robotName+bakeryName;
+		Game.bakeryNamePrompt();
 		Game.ConfirmPrompt();
 	}
 }
 
-BakingBot.NameItBack = function(){ 
+BakingBot.NameItBack = function(){
 		Game.bakeryName = BakingBot.InitialBakeryName;
 		Game.bakeryNamePrompt();
 		Game.ConfirmPrompt();
@@ -301,30 +337,57 @@ BakingBot.CanIBuyBTen = function(id){
 	return false;
 }
 
+BakingBot.LoadCSS=function(url){
+	var fileref=document.createElement('link');
+	var id=url.split('/');id=id[id.length-1].split('.')[0];
+	fileref.setAttribute("rel", "stylesheet")
+	fileref.setAttribute("type", "text/css")
+	fileref.setAttribute("href", url)
+	fileref.setAttribute('id','cssmod_'+id);
+
+	document.head.appendChild(fileref);
+	console.log('Loaded the CSS file from '+url+', ID name: '+id);
+}
+
+BakingBot.TimeConverter = function(UNIX_timestamp)	{
+	//Ty random guy on https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
+	var a = new Date(UNIX_timestamp);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+}
+
 //---------Init--------
 
 BakingBot.Inits = function(){
-	
+
 	var proceed = true;
 	if(Game.prestige == 0)
 		proceed = confirm('Seems like you are at your first run \nThis bot CAN RESET your progress by ascending.\nDo you still want to load BakingBot?');
 	if (proceed) {
 		BakingBot.starter = setInterval(BakingBot.run,300);
-		
+
 		if(Game.version == BakingBot.gameVersion){
 			if (Game.prefs.popups) Game.Popup('BakingBot v.'+BakingBot.version+' for version ' + BakingBot.gameVersion + ' loaded!');
 			else BakingBot.notify('BakingBot v.'+BakingBot.version+' for version ' + BakingBot.gameVersion + ' loaded!');
-		}else{ 
+		}else{
 			Game.Notify("BakingBot News","Warning: BakingBot is last tested with "+"cookie clicker version " + BakingBot.gameVersion,[15,5],100);
 		}
-		
+
 		Game.Win('Third-party');
 		BakingBot.RenameBakery();
-		
+
 	}
 }
 
 BakingBot.stopBot = function(){
+	BakingBot.SavingList("Success");
 	clearInterval(BakingBot.AutoClicker);
 	clearInterval(BakingBot.starter);
 	BakingBot.NameItBack();
--- README.md
+++ README.md
@@ -1,3 +1,87 @@
 # BakingBot
-BakingBot is a CookieCliker bot
-still WIP
+**BakingBot** is an add-on you can load into Cookie Clicker, that will unlock the "Speed baking III" achievements for you.
+To get "Speed baking III" you dont need an actually strategy, you only need luck (or a lot of time).
+
+## How it work?
+BakingBot will automatically ascend if you have more then 1M cookies in the bakery.Then it will set the gamemode in to "Born again" mode and reincarnate.
+BB then will start clicking the BigCookie,goldencookie,buying building and upgrade untill min 15 (and 10 seconds).
+Then,it will start the cycle over again.
+
+## Limitations
+The buying strategy used in this bot its not the best possible.
+I have used my experience to create a reasonable path,and it accually worked.  
+In the options there is the possibility to choose the speed of clicking.
+Even if it is present, it is not recommended to choose "3",it will probably removed in some new versions.
+
+# Usage
+
+## Bookmarklet
+Copy this code and save it as a bookmark. Paste it in the URL section. To activate, click the bookmark when the Cookie Clicker game is open.
+
+```javascript
+javascript: (function () {
+	Game.LoadMod('https://atomicdonuts.github.io/BakingBot/BakingBot.js');
+}());
+```
+You can also try the beta version with the following code(not recommended).
+
+## Userscript
+
+BakingBot can also be activeted via script in *Tampermonkey* (or *Greasemonkey*).This script will automatically load *BakingBot* every time the original game loads. Check your browsers/plugin's documentation for how to add a userscript. This script is courtesy of **[SearchAndDestroy](https://github.com/SearchAndDestroy)**.
+
+```javascript
+// ==UserScript==
+// @name        BakingBot
+// @namespace   https://github.com/AtomicDonuts/BakingBot
+// @include     http://orteil.dashnet.org/cookieclicker/
+// @include     https://orteil.dashnet.org/cookieclicker/
+// @version     0.3
+// @author      AtomicDonuts
+// @grant       none
+// ==/UserScript==
+
+var code = "(" + (function() {
+    var checkReady = setInterval(function() {
+        if (typeof Game.ready !== 'undefined' && Game.ready) {
+            Game.LoadMod('https://atomicdonuts.github.io/BakingBot/BakingBot.js');
+            clearInterval(checkReady);
+        }
+    }, 1000);
+}).toString() + ")()";
+
+window.eval(code);
+```
+
+If you want to use the beta, use this one instead:
+
+```javascript
+// ==UserScript==
+// @name        BakingBotBeta
+// @namespace   https://github.com/AtomicDonuts/BakingBot
+// @include     http://orteil.dashnet.org/cookieclicker/
+// @include     https://orteil.dashnet.org/cookieclicker/
+// @version     Unknown
+// @author      AtomicDonuts
+// @grant       none
+// ==/UserScript==
+
+var code = "(" + (function() {
+    var checkReady = setInterval(function() {
+        if (typeof Game.ready !== 'undefined' && Game.ready) {
+            Game.LoadMod('https://atomicdonuts.github.io/BakingBot/BakingBotBeta.js');
+            clearInterval(checkReady);
+        }
+    }, 1000);
+}).toString() + ")()";
+
+window.eval(code);
+```
+
+# Bugs and suggestions
+Since I'm not a programmer,the script probably have a bad syntax and have some bugs.
+If you have suggestions for alternative strategy or just want to report some bug, please contact me at Pascal98@hotmail.it .
+
+# Contributors
+* **[AtomicDonuts](https://github.com/AtomicDonuts)**: Original author
+* **[SearchAndDestroy](https://github.com/SearchAndDestroy)**: Tampermonkey script
+* **[Prinz Stani](https://github.com/prinzstani)**: Cookie Bot original author, i used his script as base for mine
